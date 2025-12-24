8da2e2c1c29df10967e98f1358769688b9e63c2e
Marco Deb <marco.deb@gmail.com>
2025-12-24 21:24:52 +0100

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Marco Deb on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
